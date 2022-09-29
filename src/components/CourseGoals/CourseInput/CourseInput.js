a38a63eca815889409057295b86f01bc028ac9d1
Ajay Kumar Mandal <ajaykumar.mandal@oodles.io>
2022-09-29 14:56:59 +0530

    added css module in js files

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,49 +1,50 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-import styled from 'styled-components';
+import styles from  './CourseInput.module.css';
+// import styled from 'styled-components';
 
-const FormControl=styled.div`
-  margin: 0.5rem 0;
+// const FormControl=styled.div`
+//   margin: 0.5rem 0;
 
-#label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color:${props=> props.invalid ? "red":"black"
-  }
-}
+// #label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color:${props=> props.invalid ? "red":"black"
+//   }
+// }
 
-#input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props=>props.invalid? "red":"#ccc"
-  };
-  // border: 1px solid #ccc;
-  background:${props=>
-    props.invalid ? "#fad0ec" :"transparent"
-  }
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// #input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props=>props.invalid? "red":"#ccc"
+//   };
+//   // border: 1px solid #ccc;
+//   background:${props=>
+//     props.invalid ? "#fad0ec" :"transparent"
+//   }
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-
-// &.invalid input {
-//   border-color: red;
+// & input:focus {
+//   outline: none;
 //   background: #fad0ec;
-// }
-// &.invalid label {
-//   color: red;
+//   border-color: #8b005d;
 // }
 
-`
+// // &.invalid input {
+// //   border-color: red;
+// //   background: #fad0ec;
+// // }
+// // &.invalid label {
+// //   color: red;
+// // }
+
+// `
+
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid,setIsValid]=useState(true)
@@ -71,16 +72,18 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      {/* <FormControl invalid={!isValid}> */}
       {/* <FormControl className={`form-control ${!isValid? "invalid":""}`}> */}
-        <label id="label">Course Goal</label>
+        {/* <label id="label">Course Goal</label>
         <input id="input" type="text" onChange={goalInputChangeHandler} value ={enteredValue}/>
         </FormControl>
-     
-      {/* <div className={`form-control ${!isValid? "invalid":""}`}>
+      */}
+      
+
+      <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} value ={enteredValue}/>
-      </div> */}
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
